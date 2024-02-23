d161541ff0c492a068cfc8b4824c9c49449064ef
insv <insv23@outlook.com>
2024-02-23 17:49:38 +0800

    part3: finished

--- models/note.js
+++ models/note.js
@@ -1,31 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const dbUrl = process.env.MONGODB_URI;
+const dbUrl = process.env.MONGODB_URI
 
-console.log("connecting to", dbUrl);
+console.log('connecting to', dbUrl)
 
 mongoose
   .connect(dbUrl)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((err) => {
-    console.log("error connecting to MongoDB:", err.message);
-  });
+    console.log('error connecting to MongoDB:', err.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
   },
-});
+  important: Boolean,
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Note', noteSchema)
